41c9f8ca07db72d3a36310789c1dfe4025fe1980
codebloo <hilary@codebloo.com>
2016-01-05 22:03:35 -0800

    POST ID

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <div class="container">
 <section class="columns twelve">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<?php the_content(); ?>
+<article class="post" id="post-<?php the_ID(); ?>">
+<h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+<?php the_excerpt(); ?></article>
 <?php endwhile; endif; ?>
 </section>
 <aside class="columns four">
