575f0ee7766c0bee3097821706f57d4f28e194af
codebloo <hilary@codebloo.com>
2016-01-09 20:58:25 -0800

    Adding ARIA Landmark Roles

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
 <div class="container">
-<section class="columns twelve">
+<main class="columns twelve" role="main">
 	<h2>Blog</h2>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <article class="post" id="post-<?php the_ID(); ?>">
 <h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
 <?php the_excerpt(); ?></article>
 <?php endwhile; endif; ?>
-</section>
-<aside class="columns four">
+</main>
+<aside class="columns four" role="complementary">
 	<div id="sidebar">
 	<?php dynamic_sidebar('Blog');?>
 <div>
--- single.php
+++ single.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
-<section class="twelve columns">
+<main class="twelve columns" role="main">
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 		<h3><?php the_title();?></h3>
 		<?php the_content(); ?>
 
 	<?php endwhile; endif; ?>
-</section>
-<aside class="four columns">
+</main>
+<aside class="four columns" role="complementary">
 <?php get_sidebar (); ?>
 </aside>
 
